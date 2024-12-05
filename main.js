b9fa33696ac4308133ce0df50ee377f8db023566
fath-ali <fathalishah208@gmail.com>
2024-12-05 12:09:05 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// function myfunction(fullimage){
+//     let acess = document.getElementById("imagebox");
+//     console.log(acess);
+    
+//     acess.src = fullimage.src;
+// }
+function myfunction(fullimage){
+    let show =document.getElementById("imagebox");
+    show.src = fullimage.src;
+}
